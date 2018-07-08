7c8a42874528437d39cb1be27edc8498eaa77524
ste ste <steelst0@gmail.com>
2018-07-07 17:29:00 -0700

    functional

--- dojo_survey.py
+++ dojo_survey.py
@@ -10,13 +10,22 @@ def index():
 #http://localhost:5000/result - have this display a html with the information that was submitted by POST
 @app.route('/result', methods=['POST'])
 def result():
+    # Set up a boolean to let us know if we hit an error or not
+    error = False
     if len(request.form['name']) < 1:
-        flash("Name field cannot be empty!")
+        flash("Name is required!")
+        error = True
     if len(request.form['comment']) < 1:
         flash("Comment field cannot be empty!")
+        error = True
+    if len(request.form['comment']) > 120:
+        flash("Comment length must be less than 120!")
+        error = True
+    # If the boolean has been toggled to true we return the user to the index
+    if error == True:
         return redirect('/')
     else:
-        return render_template('result.html')
+        return render_template('/result.html')
 
 #http://localhost:5000/danger - have this redirect back to "/".
 #Before redirecting back print in the terminal/console a message: 
